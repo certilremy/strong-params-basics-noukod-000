cfe8eed07dc70a7f86768e5ff8762e606cf816df
Certil Remy <certil@noukod.com>
2019-01-22 17:25:27 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   #@post = Post.new(post_params)
   #@post.save
   #redirect_to post_path(@post)
-#end
+  #end
 
 	def create
 	  @post = Post.new(params["post"])
